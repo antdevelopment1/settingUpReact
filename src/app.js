4e7d588b97c6d456c096dd0b2cef1baa4a66682c
April Copes <antdevelopment1@gmail.com>
2018-12-03 18:59:14 -0500

    rendered babel code

--- src/app.js
+++ src/app.js
@@ -2,7 +2,27 @@ console.log('App.js is running');
 
 // JSX - JavaScript XML
 
-var template = <h1>Indecision App</h1>
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is a lot of fun</p>
+        <ol>
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+        </ol>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>April Copes</h1>
+        <p>Age: 26</p>
+        <p>Location: New York</p>
+    </div>
+)
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+
+ReactDOM.render(templateTwo, appRoot);
