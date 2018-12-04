31e313c73a80f31616bbe0c9f74ba3f30afc92ca
April Copes <antdevelopment1@gmail.com>
2018-12-03 19:00:50 -0500

    rendered babel code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,10 +5,59 @@ console.log('App.js is running');
 // JSX - JavaScript XML
 
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is a lot of fun'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'List item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'List item 2'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'List item 3'
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'April Copes'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 26'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: New York'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
