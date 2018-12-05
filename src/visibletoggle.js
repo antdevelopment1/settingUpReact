fbfc8fdd57a98edd937784504257acb3cfb6c8ec
April Copes <antdevelopment1@gmail.com>
2018-12-05 14:04:23 -0500

    creates new file for visble toggle button

---
+++ src/visibletoggle.js
@@ -0,0 +1,31 @@
+console.log('App.js is running');
+
+let visibility = false;
+
+const toggleVisibility = () => {
+    visibility = !visibility;
+    render();
+}
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+
+    const template = (
+        <div>
+            <h1>Visibility Toggle</h1>
+            <button onClick={toggleVisibility}>
+            {visibility ? 'Hide details' : 'Show Details'}
+            </button>
+            {visibility && (
+                <div>
+                    <p>Hey. These are some deatils for you.</p>
+                </div>
+            )}
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+}
+
+render();
