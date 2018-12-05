049c4b61af9cc9ad6378f98faa7643890ee4cb0c
April Copes <antdevelopment1@gmail.com>
2018-12-05 11:51:32 -0500

    Creates file for indesision app and moves file from src/app.js

---
+++ src/playground/indesisionApp.js
@@ -0,0 +1,67 @@
+console.log('App.js is running');
+
+// JSX - JavaScript XML
+
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Going to go make a desision',
+    options: []
+};
+
+const onFormSubmit = (e) => {
+
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderSubmitOption();
+    }
+
+}
+
+const onRemoveAll = () => {
+    app.options = [];
+    renderSubmitOption();
+}
+
+const onMakeDesision = () => {
+    const randomNumber = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNumber];
+    alert(option);
+};
+
+// Create remove all button abouve list
+// On click wipe the array an re render
+
+const appRoot = document.getElementById('app');
+
+const renderSubmitOption = () => {
+
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options available'}</p>
+            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDesision}>What should I do?</button>
+            <button onClick={onRemoveAll}>Remove All</button>
+            <ol>
+            {
+                app.options.map( option => {
+                    return <li key={option}>{option}</li>
+                })
+            }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+}
+
+renderSubmitOption();
