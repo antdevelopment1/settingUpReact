563e8709401708f9975d261b8fedc7d1806708eb
April Copes <antdevelopment1@gmail.com>
2018-12-05 11:50:54 -0500

    Adds simple button and toggle feature

--- src/app.js
+++ src/app.js
@@ -1,71 +1,32 @@
 console.log('App.js is running');
 
-// JSX - JavaScript XML
+let visibility = false;
 
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Going to go make a desision',
-    options: []
-};
-
-const onFormSubmit = (e) => {
-
-    e.preventDefault();
-    const option = e.target.elements.option.value;
-
-    if (option) {
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        renderSubmitOption();
-    }
-
+const toggleVisibility = () => {
+    visibility = !visibility;
+    render();
 }
-
-const onRemoveAll = () => {
-    app.options = [];
-    renderSubmitOption();
-}
-
-const onMakeDesision = () => {
-    const randomNumber = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNumber];
-    alert(option);
-};
-
-// Create remove all button abouve list
-// On click wipe the array an re render
 
 const appRoot = document.getElementById('app');
 
-const renderSubmitOption = () => {
+const render = () => {
 
     const template = (
         <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length > 0 ? 'Here are your options' : 'No options available'}</p>
-            <p>{app.options.length}</p>
-            <button disabled={app.options.length === 0} onClick={onMakeDesision}>What should I do?</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-            <ol>
-            {
-                app.options.map( option => {
-                    return <li key={option}>{option}</li>
-                })
-            }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option" />
-                <button>Add Option</button>
-            </form>
+            <h1>Visibility Toggle</h1>
+            <button onClick={toggleVisibility}>
+            {visibility ? 'Hide details' : 'Show Details'}
+            </button>
+            {visibility && (
+                <div>
+                    <p>Hey. These are some deatils for you.</p>
+                </div>
+            )}
         </div>
     );
 
     ReactDOM.render(template, appRoot);
 }
 
-renderSubmitOption();
-
-
-
+render();
 
